904fd3d2ee62ae44ee72d03ff52d534aa2462ac4
sunghyun Kim <constate93@gmail.com>
2024-03-22 21:32:00 +0900

    HomePage add

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const HomePage = () => {
+    return <HomePageWrap></HomePageWrap>;
+};
+
+const HomePageWrap = styled.section`
+    position: relative;
+    width: 1280px;
+    margin: 0 auto;
+    padding-bottom: 60px;
+    background-color: chocolate;
+`;
