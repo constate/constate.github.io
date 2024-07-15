13cf51d9cb3031f3a3845905668ee6362aaf34fb
sunghyun Kim <constate93@gmail.com>
2024-07-15 21:08:41 +0900

    GolobalStyles add & file clear

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,51 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+export const GlobalStyle = createGlobalStyle`
+    ${reset};
+    * {
+        box-sizing: border-box;
+    }    
+    html, body {
+        min-width: 1280px;
+        background-color: #FFF;
+        overflow-x: auto;
+        overflow-y: hidden;
+    }
+    html, body, div, span, h1, h2, h3, h4, h5, h6, p, input, textarea,
+    a, dl, dt, dd, ol, ul, li, form, label, table, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, figcaption, figure{
+        margin: 0;
+        padding: 0;
+        border: 0;
+        font-size: 10px;
+        
+        &::-webkit-scrollbar {
+            width: 1.2rem;
+            height: 1.2rem;
+        }
+        &::-webkit-scrollbar-track {
+            background-color: transparent;
+            border-radius: 1.2rem;
+            
+        }
+        &::-webkit-scrollbar-thumb {
+            background-color: #d3d3d3;
+            border-radius: 1.2rem;
+            height: 10rem;
+        }
+    }
+    a{
+        text-decoration: none;
+        color: inherit;
+    }
+    ol, ul{
+        list-style: none;
+    }
+    button {
+        border: 0;
+        padding: 0;
+        background: transparent;
+        cursor: pointer;
+    }
+    
+
+`;
