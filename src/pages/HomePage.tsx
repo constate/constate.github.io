ddaf35ce89609582ad95460bf4c825ef0634df8e
sunghyun Kim <constate93@gmail.com>
2024-07-17 18:13:48 +0900

    qrcode.react module add

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { QRCodeSVG } from 'qrcode.react';
 
 interface Props {}
 
 export const HomePage = (props: Props) => {
-    return <div>HomePage</div>;
+    return (
+        <>
+            <QRCodeSVG value="https://github.com/constate" />
+        </>
+    );
 };
